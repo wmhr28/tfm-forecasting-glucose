56dba68e221f5bb801ea6a945acbc65346c9da96
Mauricio Herrera <wmhr28@hotmail.com>
2023-02-18 11:04:14 -0500

    balanceo y entrenamiento transfer learning

--- codigo/model_LSTM.py
+++ codigo/model_LSTM.py
@@ -75,3 +75,14 @@ def modelLSTM_Gen_Bidirectional_nodrop(dim1,dim2):
     model.add(Dense(1))
     model.compile(optimizer = Adam(learning_rate=0.001), loss='mse', metrics=['mae','mse'])
     return model
+
+def transferLearning_NewModel(base_model,lastLayer,includeDropout):
+    base_model.trainable = False 
+    x = base_model.layers[lastLayer].output 
+    x = Dense(32, activation='relu',name='newLayer')(x)
+    if(includeDropout):
+        x = Dropout(0.2,name='newDropout')(x) 
+    
+    newOutput = Dense(1,name='newOutput')(x) 
+        
+    return keras.Model(inputs=base_model.input, outputs=newOutput)
